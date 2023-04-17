d60a7246dfdf9aec0b937e069ed7b7f7e95062f3
TheOnlySD12 <103511260+TheOnlySD12@users.noreply.github.com>
2023-04-17 21:44:44 +0300

    Update a lot of stuff

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
+[<img alt="forthebadge" height="50" src="https://forthebadge.com/images/badges/gluten-free.svg" width="250"/>](https://forthebadge.com)
+[<img alt="forthebadge" height="50" src="https://forthebadge.com/images/badges/built-with-swag.svg" width="250"/>](https://forthebadge.com)
+[<img alt="forthebadge" height="50" src="https://forthebadge.com/images/badges/works-on-my-machine.svg" width="250"/>](https://forthebadge.com)
 # IPLogin
-### Basic PaperMC plugin for IP login
-When you first log in, the plugin stores your username and IP, and the next time someone joins with your username, if the IP is correct, nothing happens, but if the IP is incorrect, it gets banned.
+**IPLogin** is a PaperMC plugin that allows server owners to restrict access to their servers based on IP address.
+## Features
+- Restrict server access based on IP address
+    - Ban/Kick alts or impersonators
+- Option to allow specific IP addresses to bypass the restriction
+    - Players with a dynamic IP
+    - Players that often login from other locations
+- Easy setup and configuration
+## Getting Started
+To use **IPLogin**, simply download and install the version of **IPLogin** for your PaperMC server's version. Note that currently there is only one version; the one for `1.19-1.19.4`.
 
-Alts are also banned, if allowalts is false and a new player has the IP of an already existing player, the IP gets banned.
+When a player first joins the server, their IP will be associated with their username. From now on, the plugin will check anyone with that player's username to see if they match the store IP.
 
-The command /setip <newip> changes your stored IP to a new one. After you log out, this IP will be used to verify your logins.
+By default, **IPLogin** will kick all players who do not have a matching IP address in the `name-to-ip-map` file. You can add an `*` next to a player's name to allow them to access the server with any IP.
+## Configuration
+Once you've installed the plugin, you can configure it by editing the `config.yml` file, which is located in `plugins/IPLogin`. The file should look like this:
+```
+allow-alts: false
+ban-alts: false
+alts-message: Alts are not allowed.
+ban-impersonators: false
+impersonators-message: Impersonators are not allowed.
+welcome-back-message: 'Welcome back, '
+welcome-message: 'Welcome, '
+```
+Here you can change the messages for alts and impersonators, you can make the plugin kick or ban impersonators or kick or ban alts, you can even allow alts.
+## Security
+**IPLogin** provides a simple and effective way to restrict server access based on IP address. However, it is important to remember that IP addresses can be easily changed. For example, someone can use _a free VPN_ to change their IP **to a random one**. Therefore, **IPLogin** should not be relied upon as the sole method of securing your server, you should also have moderators.
+## Support
+If you have any questions or issues with **IPLogin**, you can use [the issue tracker](https://github.com/TheOnlySD12/iplogin/issues) on GitHub.
+## Credits
+**IPLogin** was developed by [TheOnlySD12](https://github.com/TheOnlySD12) and is licensed under the **MIT License**. Special thanks to the following people for their contributions to the project:
 
-The command /allowalts <value> allows/disallows alts. Alts that login after this command is executed will be banned.
+- Me (Code Review)
+- Myself (Ideas)
+- I (Testing)
 
-You can also change allow-alts from the config file.
+I hope you enjoy using **IPLogin**! Please don't hesitate to use [the issue tracker](https://github.com/TheOnlySD12/iplogin/issues) if you have any feedback or suggestions.
--- src/main/java/com/theonlysd12/iplogin/IPLogin.java
+++ src/main/java/com/theonlysd12/iplogin/IPLogin.java
@@ -2,8 +2,10 @@ package com.theonlysd12.iplogin;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -24,20 +26,62 @@ public class IPLogin extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("setip") && args.length == 1) {
-            if (sender instanceof Player player) {
-               NameToIPMap.put(player.getName(), args[0]);
-               player.sendMessage(Component.text("Your IP has been set to: ", NamedTextColor.YELLOW)
-                       .append(Component.text(args[0], NamedTextColor.WHITE)));
-               return true;
+        if (command.getName().equalsIgnoreCase("setip")) {
+            if (args.length == 1 && sender instanceof Player player) {
+                NameToIPMap.put(player.getName(), args[0]);
+                player.sendMessage(Component.text("Your IP has been set to ", NamedTextColor.YELLOW)
+                        .append(Component.text(args[0], NamedTextColor.WHITE)));
+                return true;
+            } else if (args.length == 1 && sender instanceof ConsoleCommandSender || args.length == 1 && sender instanceof BlockCommandSender) {
+                getLogger().info("You must be a player to use this command.");
+                return true;
+            }
+            if (args.length == 2) {
+                if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender) {
+                    NameToIPMap.put(args[0], args[1]);
+                    getLogger().info(args[0] + "'s IP has been set to: " + args[1]);
+                    return true;
+                } else if (sender instanceof Player player && sender.isOp()) {
+                    NameToIPMap.put(args[0], args[1]);
+                    player.sendMessage(Component.text(args[0] + "'s IP has been set to ", NamedTextColor.YELLOW)
+                            .append(Component.text(args[1], NamedTextColor.WHITE)));
+                    return true;
+                } else if (sender instanceof Player player) {
+                    if (Objects.equals(args[0], player.getName())) {
+                        NameToIPMap.put(args[0], args[1]);
+                        player.sendMessage(Component.text("Your IP has been set to ", NamedTextColor.YELLOW)
+                                .append(Component.text(args[1], NamedTextColor.WHITE)));
+                        return true;
+                    }
+                    player.sendMessage(Component.text("You do not have permission to do that.", NamedTextColor.RED));
+                    return true;
+                }
             }
         }
         if (command.getName().equalsIgnoreCase("allowalts") && args.length == 1) {
-            fileConfig.set("allow-alts", Boolean.parseBoolean(args[0]));
-            if (sender instanceof Player player) {
-                player.sendMessage(Component.text("Allow-Alts is now set to ", NamedTextColor.YELLOW).append(Component.text(Boolean.parseBoolean(args[0]), NamedTextColor.WHITE)));
+            if (args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("false")) {
+                if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender) {
+                    fileConfig.set("allow-alts", Boolean.parseBoolean(args[0]));
+                    getLogger().info("Allow-Alts is now set to " + args[0]);
+                    return true;
+                } else if (sender instanceof Player player && sender.isOp()) {
+                    fileConfig.set("allow-alts", Boolean.parseBoolean(args[0]));
+                    player.sendMessage(Component.text("Allow-Alts is now set to ", NamedTextColor.YELLOW)
+                            .append(Component.text(args[0], NamedTextColor.WHITE)));
+                    return true;
+                } else if (sender instanceof Player player) {
+                    player.sendMessage(Component.text("You do not have permission to do that.", NamedTextColor.RED));
+                    return true;
+                }
             }
-            return true;
+        } else if (command.getName().equalsIgnoreCase("allowalts") && args.length == 0) {
+            if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender) {
+                getLogger().info("Allow-Alts is currently set to " + fileConfig.getBoolean("allow-alts"));
+                return true;
+            } else if (sender instanceof Player player) {
+                player.sendMessage(Component.text("Allow-Alts is currently set to " + fileConfig.getBoolean("allow-alts"), NamedTextColor.YELLOW));
+                return true;
+            }
         }
         return false;
     }
@@ -45,6 +89,12 @@ public class IPLogin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         fileConfig.addDefault("allow-alts", false);
+        fileConfig.addDefault("ban-alts", false);
+        fileConfig.addDefault("alts-message", "Alts are not allowed.");
+        fileConfig.addDefault("ban-impersonators", false);
+        fileConfig.addDefault("impersonators-message", "Impersonators are not allowed.");
+        fileConfig.addDefault("welcome-back-message", "Welcome back, ");
+        fileConfig.addDefault("welcome-message","Welcome, ");
         fileConfig.options().copyDefaults(true);
         saveConfig();
         File dataFolder = getDataFolder();
@@ -71,18 +121,24 @@ public class IPLogin extends JavaPlugin implements Listener {
         String name = event.getPlayer().getName();
         String ip = Objects.requireNonNull(event.getPlayer().getAddress()).getAddress().getHostAddress();
         if (NameToIPMap.containsKey(name)) {
-            if (!NameToIPMap.get(name).equals(ip)) {
-                event.getPlayer().banPlayerIP("You tried to enter with someone else's username");
-            } else {
-                event.joinMessage(Component.text("Welcome back, ", NamedTextColor.YELLOW)
+            if (NameToIPMap.get(name).equals("*") || NameToIPMap.get(name).equals(ip)) {
+                event.joinMessage(Component.text(Objects.requireNonNull(fileConfig.getString("welcome-back-message")), NamedTextColor.YELLOW)
                         .append(Component.text(name, NamedTextColor.YELLOW)));
+            } else if (fileConfig.getBoolean("ban-impersonators")) {
+                event.getPlayer().banPlayerIP(fileConfig.getString("impersonators-message"));
+            } else {
+                event.getPlayer().kick(Component.text(Objects.requireNonNull(fileConfig.getString("impersonators-message"))));
             }
         } else {
-            if(NameToIPMap.containsValue(ip) && fileConfig.getBoolean("allow-alts")) {
-                event.getPlayer().banPlayerIP("Alts are not allowed");
+            if(NameToIPMap.containsValue(ip) && !fileConfig.getBoolean("allow-alts")) {
+                if (fileConfig.getBoolean("ban-alts")) {
+                    event.getPlayer().banPlayerIP(fileConfig.getString("alts-message"));
+                } else {
+                    event.getPlayer().kick(Component.text(Objects.requireNonNull(fileConfig.getString("alts-message"))));
+                }
             } else {
                 NameToIPMap.put(name, ip);
-                event.joinMessage(Component.text("Welcome, ", NamedTextColor.YELLOW)
+                event.joinMessage(Component.text(Objects.requireNonNull(fileConfig.getString("welcome-message")), NamedTextColor.YELLOW)
                         .append(Component.text(name, NamedTextColor.YELLOW)));
             }
         }
